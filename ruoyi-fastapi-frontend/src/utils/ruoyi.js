8fcd4a2181aad646b68760b438e4cef97a4cf22b
insistence <3055204202@qq.com>
2024-01-24 22:17:40 +0800

    feat: 完成各模块列表接口联调

--- ruoyi-fastapi-frontend/src/utils/ruoyi.js
+++ ruoyi-fastapi-frontend/src/utils/ruoyi.js
@@ -56,14 +56,13 @@ export function resetForm(refName) {
 // 添加日期范围
 export function addDateRange(params, dateRange, propName) {
   let search = params;
-  search.params = typeof (search.params) === 'object' && search.params !== null && !Array.isArray(search.params) ? search.params : {};
   dateRange = Array.isArray(dateRange) ? dateRange : [];
   if (typeof (propName) === 'undefined') {
-    search.params['beginTime'] = dateRange[0];
-    search.params['endTime'] = dateRange[1];
+    search['beginTime'] = dateRange[0];
+    search['endTime'] = dateRange[1];
   } else {
-    search.params['begin' + propName] = dateRange[0];
-    search.params['end' + propName] = dateRange[1];
+    search['begin' + propName] = dateRange[0];
+    search['end' + propName] = dateRange[1];
   }
   return search;
 }
