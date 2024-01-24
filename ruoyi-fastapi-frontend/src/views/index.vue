0f2ce436ea5e331f6a7c82b023b1e0a6354fa699
insistence <3055204202@qq.com>
2024-01-24 22:21:27 +0800

    fix: 兼容首页缺少静态文件

--- ruoyi-fastapi-frontend/src/views/index.vue
+++ ruoyi-fastapi-frontend/src/views/index.vue
@@ -964,7 +964,7 @@
           </el-collapse>
         </el-card>
       </el-col>
-      <el-col :xs="24" :sm="24" :md="12" :lg="8">
+      <!-- <el-col :xs="24" :sm="24" :md="12" :lg="8">
         <el-card class="update-log">
           <template v-slot:header>
             <div class="clearfix">
@@ -982,7 +982,7 @@
             >
           </div>
         </el-card>
-      </el-col>
+      </el-col> -->
     </el-row>
   </div>
 </template>
