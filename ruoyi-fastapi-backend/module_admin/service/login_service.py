2a45df71cdaeda0ff8deadba8fe69858239e46cd
insistence <3055204202@qq.com>
2024-03-04 15:49:05 +0800

    fix: 修复外链菜单无法打开的问题 #I95KBK

--- ruoyi-fastapi-backend/module_admin/service/login_service.py
+++ ruoyi-fastapi-backend/module_admin/service/login_service.py
@@ -245,13 +245,15 @@ class LoginService:
                 if permission.menu_type == 'M':
                     router_list_data['name'] = permission.path.capitalize()
                     router_list_data['hidden'] = False if permission.visible == '0' else True
-                    if permission.is_frame == 1:
-                        router_list_data['redirect'] = 'noRedirect'
                     if permission.parent_id == 0:
                         router_list_data['component'] = 'Layout'
                         router_list_data['path'] = f'/{permission.path}'
                     else:
                         router_list_data['component'] = 'ParentView'
+                        router_list_data['path'] = permission.path
+                    if permission.is_frame == 1:
+                        router_list_data['redirect'] = 'noRedirect'
+                    else:
                         router_list_data['path'] = permission.path
                     if children:
                         router_list_data['alwaysShow'] = True
