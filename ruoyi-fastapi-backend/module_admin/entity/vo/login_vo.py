43f903c6e0878fa012ea5b19ebf9c4ddae3bd2ff
insistence <3055204202@qq.com>
2024-06-27 21:51:01 +0800

    fix: 修复菜单挂载到根目录时路由加载异常等一系列相关问题

--- ruoyi-fastapi-backend/module_admin/entity/vo/login_vo.py
+++ ruoyi-fastapi-backend/module_admin/entity/vo/login_vo.py
@@ -1,8 +1,9 @@
 import re
 from pydantic import BaseModel, ConfigDict, model_validator
 from pydantic.alias_generators import to_camel
-from typing import Optional
+from typing import Optional, List, Union
 from exceptions.exception import ModelValidatorException
+from module_admin.entity.vo.menu_vo import MenuModel
 
 
 class UserLogin(BaseModel):
@@ -53,3 +54,30 @@ class SmsCode(BaseModel):
     sms_code: str
     session_id: str
     message: Optional[str] = None
+
+
+class MenuTreeModel(MenuModel):
+    children: Optional[Union[List['MenuTreeModel'], None]] = None
+
+
+class MetaModel(BaseModel):
+    model_config = ConfigDict(alias_generator=to_camel)
+
+    title: Optional[str] = None
+    icon: Optional[str] = None
+    no_cache: Optional[bool] = None
+    link: Optional[str] = None
+
+
+class RouterModel(BaseModel):
+    model_config = ConfigDict(alias_generator=to_camel)
+
+    name: Optional[str] = None
+    path: Optional[str] = None
+    hidden: Optional[bool] = None
+    redirect: Optional[str] = None
+    component: Optional[str] = None
+    query: Optional[str] = None
+    always_show: Optional[bool] = None
+    meta: Optional[MetaModel] = None
+    children: Optional[Union[List['RouterModel'], None]] = None
