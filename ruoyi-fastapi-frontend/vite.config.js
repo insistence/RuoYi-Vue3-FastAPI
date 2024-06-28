2cf5268f7ddf5df6aebec70c6a17e1cd0c2273d6
insistence <3055204202@qq.com>
2024-06-28 14:31:22 +0800

    perf: 修改代理localhost为127.0.0.1以适配部分设备解析localhost异常的问题

--- ruoyi-fastapi-frontend/vite.config.js
+++ ruoyi-fastapi-frontend/vite.config.js
@@ -31,7 +31,7 @@ export default defineConfig(({ mode, command }) => {
       proxy: {
         // https://cn.vitejs.dev/config/#server-proxy
         '/dev-api': {
-          target: 'http://localhost:9099',
+          target: 'http://127.0.0.1:9099',
           changeOrigin: true,
           rewrite: (p) => p.replace(/^\/dev-api/, '')
         }
