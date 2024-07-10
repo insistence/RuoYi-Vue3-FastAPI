55babcee4c1e8dd937875905124e18828ec83df0
insistence <3055204202@qq.com>
2024-07-10 20:47:51 +0800

    perf: 优化服务监控模块异常处理

--- ruoyi-fastapi-backend/module_admin/controller/server_controller.py
+++ ruoyi-fastapi-backend/module_admin/controller/server_controller.py
@@ -12,11 +12,8 @@ serverController = APIRouter(prefix='/monitor/server', dependencies=[Depends(Log
 
 @serverController.get("", response_model=ServerMonitorModel, dependencies=[Depends(CheckUserInterfaceAuth('monitor:server:list'))])
 async def get_monitor_server_info(request: Request):
-    try:
-        # 获取全量数据
-        server_info_query_result = await ServerService.get_server_monitor_info()
-        logger.info('获取成功')
-        return ResponseUtil.success(data=server_info_query_result)
-    except Exception as e:
-        logger.exception(e)
-        return ResponseUtil.error(msg=str(e))
+    # 获取全量数据
+    server_info_query_result = await ServerService.get_server_monitor_info()
+    logger.info('获取成功')
+
+    return ResponseUtil.success(data=server_info_query_result)
