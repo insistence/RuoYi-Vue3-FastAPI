380693c66d6c98b44c40a3c99c54c505cc90b502
insistence <3055204202@qq.com>
2024-07-12 11:06:57 +0800

    style: 使用ruff格式化验证码模块，优化导入

--- ruoyi-fastapi-backend/module_admin/controller/captcha_controller.py
+++ ruoyi-fastapi-backend/module_admin/controller/captcha_controller.py
@@ -1,28 +1,41 @@
 import uuid
+from datetime import timedelta
 from fastapi import APIRouter, Request
 from config.env import RedisInitKeyConfig
-from module_admin.service.captcha_service import *
 from module_admin.entity.vo.login_vo import CaptchaCode
-from utils.response_util import *
-from utils.log_util import *
-from datetime import timedelta
+from module_admin.service.captcha_service import CaptchaService
+from utils.response_util import ResponseUtil
+from utils.log_util import logger
 
 
 captchaController = APIRouter()
 
 
-@captchaController.get("/captchaImage")
+@captchaController.get('/captchaImage')
 async def get_captcha_image(request: Request):
-    captcha_enabled = True if await request.app.state.redis.get(f"{RedisInitKeyConfig.SYS_CONFIG.get('key')}:sys.account.captchaEnabled") == 'true' else False
-    register_enabled = True if await request.app.state.redis.get(
-        f"{RedisInitKeyConfig.SYS_CONFIG.get('key')}:sys.account.registerUser") == 'true' else False
+    captcha_enabled = (
+        True
+        if await request.app.state.redis.get(f"{RedisInitKeyConfig.SYS_CONFIG.get('key')}:sys.account.captchaEnabled")
+        == 'true'
+        else False
+    )
+    register_enabled = (
+        True
+        if await request.app.state.redis.get(f"{RedisInitKeyConfig.SYS_CONFIG.get('key')}:sys.account.registerUser")
+        == 'true'
+        else False
+    )
     session_id = str(uuid.uuid4())
     captcha_result = await CaptchaService.create_captcha_image_service()
     image = captcha_result[0]
     computed_result = captcha_result[1]
-    await request.app.state.redis.set(f"{RedisInitKeyConfig.CAPTCHA_CODES.get('key')}:{session_id}", computed_result, ex=timedelta(minutes=2))
+    await request.app.state.redis.set(
+        f"{RedisInitKeyConfig.CAPTCHA_CODES.get('key')}:{session_id}", computed_result, ex=timedelta(minutes=2)
+    )
     logger.info(f'编号为{session_id}的会话获取图片验证码成功')
 
     return ResponseUtil.success(
-        model_content=CaptchaCode(captchaEnabled=captcha_enabled, registerEnabled=register_enabled, img=image, uuid=session_id)
+        model_content=CaptchaCode(
+            captchaEnabled=captcha_enabled, registerEnabled=register_enabled, img=image, uuid=session_id
+        )
     )
