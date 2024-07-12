5d8b177a12f2ae844610e5dfc15676c913852db6
insistence <3055204202@qq.com>
2024-07-12 11:35:58 +0800

    style: 使用ruff格式化server.py，优化导入

--- ruoyi-fastapi-backend/server.py
+++ ruoyi-fastapi-backend/server.py
@@ -1,43 +1,43 @@
-from fastapi import FastAPI
 from contextlib import asynccontextmanager
-from sub_applications.handle import handle_sub_applications
-from middlewares.handle import handle_middleware
-from exceptions.handle import handle_exception
-from module_admin.controller.login_controller import loginController
-from module_admin.controller.captcha_controller import captchaController
-from module_admin.controller.user_controller import userController
-from module_admin.controller.menu_controller import menuController
-from module_admin.controller.dept_controller import deptController
-from module_admin.controller.role_controller import roleController
-from module_admin.controller.post_controler import postController
-from module_admin.controller.dict_controller import dictController
-from module_admin.controller.config_controller import configController
-from module_admin.controller.notice_controller import noticeController
-from module_admin.controller.log_controller import logController
-from module_admin.controller.online_controller import onlineController
-from module_admin.controller.job_controller import jobController
-from module_admin.controller.server_controller import serverController
-from module_admin.controller.cache_controller import cacheController
-from module_admin.controller.common_controller import commonController
+from fastapi import FastAPI
 from config.env import AppConfig
-from config.get_redis import RedisUtil
 from config.get_db import init_create_table
+from config.get_redis import RedisUtil
 from config.get_scheduler import SchedulerUtil
-from utils.log_util import logger
+from exceptions.handle import handle_exception
+from middlewares.handle import handle_middleware
+from module_admin.controller.cache_controller import cacheController
+from module_admin.controller.captcha_controller import captchaController
+from module_admin.controller.common_controller import commonController
+from module_admin.controller.config_controller import configController
+from module_admin.controller.dept_controller import deptController
+from module_admin.controller.dict_controller import dictController
+from module_admin.controller.log_controller import logController
+from module_admin.controller.login_controller import loginController
+from module_admin.controller.job_controller import jobController
+from module_admin.controller.menu_controller import menuController
+from module_admin.controller.notice_controller import noticeController
+from module_admin.controller.online_controller import onlineController
+from module_admin.controller.post_controler import postController
+from module_admin.controller.role_controller import roleController
+from module_admin.controller.server_controller import serverController
+from module_admin.controller.user_controller import userController
+from sub_applications.handle import handle_sub_applications
 from utils.common_util import worship
+from utils.log_util import logger
 
 
 # 生命周期事件
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    logger.info(f"{AppConfig.app_name}开始启动")
+    logger.info(f'{AppConfig.app_name}开始启动')
     worship()
     await init_create_table()
     app.state.redis = await RedisUtil.create_redis_pool()
     await RedisUtil.init_sys_dict(app.state.redis)
     await RedisUtil.init_sys_config(app.state.redis)
     await SchedulerUtil.init_system_scheduler()
-    logger.info(f"{AppConfig.app_name}启动成功")
+    logger.info(f'{AppConfig.app_name}启动成功')
     yield
     await RedisUtil.close_redis_pool(app)
     await SchedulerUtil.close_system_scheduler()
@@ -48,7 +48,7 @@ app = FastAPI(
     title=AppConfig.app_name,
     description=f'{AppConfig.app_name}接口文档',
     version=AppConfig.app_version,
-    lifespan=lifespan
+    lifespan=lifespan,
 )
 
 # 挂载子应用
@@ -76,7 +76,7 @@ controller_list = [
     {'router': jobController, 'tags': ['系统监控-定时任务']},
     {'router': serverController, 'tags': ['系统监控-菜单管理']},
     {'router': cacheController, 'tags': ['系统监控-缓存监控']},
-    {'router': commonController, 'tags': ['通用模块']}
+    {'router': commonController, 'tags': ['通用模块']},
 ]
 
 for controller in controller_list:
