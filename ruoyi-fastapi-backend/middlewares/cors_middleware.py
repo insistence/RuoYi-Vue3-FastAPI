ca6668331fd11dfefc8b575ef248556ca8a2e2df
insistence <3055204202@qq.com>
2024-08-08 09:20:57 +0800

    style: 优化添加中间件函数注释

--- ruoyi-fastapi-backend/middlewares/cors_middleware.py
+++ ruoyi-fastapi-backend/middlewares/cors_middleware.py
@@ -3,6 +3,12 @@ from fastapi.middleware.cors import CORSMiddleware
 
 
 def add_cors_middleware(app: FastAPI):
+    """
+    添加跨域中间件
+
+    :param app: FastAPI对象
+    :return:
+    """
     # 前端页面url
     origins = [
         'http://localhost:80',
