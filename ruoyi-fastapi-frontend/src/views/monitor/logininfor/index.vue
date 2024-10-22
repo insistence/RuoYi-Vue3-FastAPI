a3c9ffdad5374c277c53ac40ce40fbdf6f05f6d2
insistence <3055204202@qq.com>
2024-10-22 19:34:13 +0800

    fix: 修复登录日志导出文件名称错误的问题

--- ruoyi-fastapi-frontend/src/views/monitor/logininfor/index.vue
+++ ruoyi-fastapi-frontend/src/views/monitor/logininfor/index.vue
@@ -218,7 +218,7 @@ function handleUnlock() {
 function handleExport() {
   proxy.download("monitor/logininfor/export", {
     ...queryParams.value,
-  }, `config_${new Date().getTime()}.xlsx`);
+  }, `logininfor_${new Date().getTime()}.xlsx`);
 }
 
 getList();
