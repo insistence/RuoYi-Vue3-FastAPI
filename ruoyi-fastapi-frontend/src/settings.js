cc6a35d7c74cb40ebe1890634b67f374cbd1d2ec
insistence <3055204202@qq.com>
2025-08-29 11:33:05 +0800

    feat: 新增页签图标显示开关功能

--- ruoyi-fastapi-frontend/src/settings.js
+++ ruoyi-fastapi-frontend/src/settings.js
@@ -8,7 +8,7 @@ export default {
    * 侧边栏主题 深色主题theme-dark，浅色主题theme-light
    */
   sideTheme: 'theme-dark',
-  
+
   /**
    * 是否系统布局配置
    */
@@ -25,6 +25,11 @@ export default {
   tagsView: true,
 
   /**
+   * 显示页签图标
+   */
+  tagsIcon: false,
+
+  /**
    * 是否固定头部
    */
   fixedHeader: false,
@@ -37,13 +42,5 @@ export default {
   /**
    * 是否显示动态标题
    */
-  dynamicTitle: false,
-
-  /**
-   * @type {string | array} 'production' | ['production', 'development']
-   * @description Need show err logs component.
-   * The default is only used in the production env
-   * If you want to also use it in dev, you can pass ['production', 'development']
-   */
-  errorLog: 'production'
+  dynamicTitle: false
 }
