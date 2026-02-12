0b6fabc13d6d7e9f3e636121447fcd9f8514333d
insistence <3055204202@qq.com>
2026-02-12 09:45:17 +0800

    fix: 修复create_app重复执行的问题

--- ruoyi-fastapi-backend/app.py
+++ ruoyi-fastapi-backend/app.py
@@ -3,14 +3,16 @@ import uvicorn
 from config.env import AppConfig
 from server import create_app
 
-app = create_app()
+if __name__ != '__main__':
+    app = create_app()
 
 if __name__ == '__main__':
     uvicorn.run(
-        app='app:app',
+        app='server:create_app',
         host=AppConfig.app_host,
         port=AppConfig.app_port,
         root_path=AppConfig.app_root_path,
         reload=AppConfig.app_reload,
         workers=AppConfig.app_workers,
+        factory=True,
     )
