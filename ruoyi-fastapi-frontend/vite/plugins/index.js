dafb553db840c392e7a1febd54d6b114b8f3cb1d
insistence <3055204202@qq.com>
2026-05-13 10:06:09 +0800

    fix: 修复前端构建异常

--- ruoyi-fastapi-frontend/vite/plugins/index.js
+++ ruoyi-fastapi-frontend/vite/plugins/index.js
@@ -6,11 +6,37 @@ import createSvgIcon from './svg-icon'
 import createCompression from './compression'
 import createSetupExtend from './setup-extend'
 
+const monacoWorkers = [
+    {
+        label: 'editorWorkerService',
+        entry: 'monaco-editor/esm/vs/editor/editor.worker.js'
+    },
+    {
+        label: 'css',
+        entry: 'monaco-editor/esm/vs/language/css/css.worker.js'
+    },
+    {
+        label: 'html',
+        entry: 'monaco-editor/esm/vs/language/html/html.worker.js'
+    },
+    {
+        label: 'json',
+        entry: 'monaco-editor/esm/vs/language/json/json.worker.js'
+    },
+    {
+        label: 'typescript',
+        entry: 'monaco-editor/esm/vs/language/typescript/ts.worker.js'
+    }
+]
+
 export default function createVitePlugins(viteEnv, isBuild = false) {
     const vitePlugins = [vue()]
     vitePlugins.push(createAutoImport())
 	vitePlugins.push(createSetupExtend())
-	vitePlugins.push(monacoEditorEsmPlugin())
+	vitePlugins.push(monacoEditorEsmPlugin({
+        languageWorkers: [],
+        customWorkers: monacoWorkers
+    }))
     vitePlugins.push(createSvgIcon(isBuild))
 	isBuild && vitePlugins.push(...createCompression(viteEnv))
     return vitePlugins
